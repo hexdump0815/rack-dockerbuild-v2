fcfe64ac41ca12b732f6003afb1fafc2ea0202af
hexdump <hexdump0815@googlemail.com>
2022-08-15 07:46:50 +0200

    simplify simde-ify.sh - use SIMDE_ENABLE_NATIVE_ALIASES
    simde has -DSIMDE_ENABLE_NATIVE_ALIASES to automatically alias
    the simde versions back to the normal names - use that instead of
    doing all this by hand

--- simde-ify.sh
+++ simde-ify.sh
@@ -4,5 +4,5 @@ MYARCH=`uname -m`
 
 # only do this on arm
 if [ "$MYARCH" = "armv7l" ] || [ "$MYARCH" = "aarch64" ]; then
-  sed -i 's,<mmintrin\.h,<x86/sse2.h,g;s,xmmintrin\.h,x86/sse2.h,g;s,pmmintrin\.h,x86/sse2.h,g;s,emmintrin\.h,x86/sse2.h,g;s,immintrin\.h,x86/sse2.h,g;s,^_m,simde_m,g;s,^__m,simde__m,g;s,\ _m, simde_m,g;s,\ __m, simde__m,g;s,\t_m,\tsimde_m,g;s,\t__m,\tsimde__m,g;s,(_m,(simde_m,g;s,(__m,(simde__m,g;s,{_mm_,{simde_mm_,g;s,<__m128,<simde__m128,g;s,\,_m,\,simde_m,g;s,\,__m,\,simde__m,g;s,~_m,~simde_m,g;s,~__m,~simde__m,g;s,:_m,:simde_m,g;s,:__m,:simde__m,g;s,\._m,.simde_m,g;s,\.__m,.simde__m,g;s,simde_mode,_mode,g;s,simde_message,_message,g;s,simde_module,_module,g;s,simde_msgCount,_msgCount,g;s,simde_mdb,_mdb,g;s,simde_m\,,_m\,,g;s,_MM_SHUFFLE,SIMDE_MM_SHUFFLE,g' "$*"
+  sed -i 's,<mmintrin\.h,<x86/sse2.h,g;s,xmmintrin\.h,x86/sse2.h,g;s,pmmintrin\.h,x86/sse2.h,g;s,emmintrin\.h,x86/sse2.h,g;s,immintrin\.h,x86/sse2.h,g;s,__m64,simde__m64,g;s,_mm_or_si64,simde_mm_or_si64,g;s,_mm_and_si64,simde_mm_and_si64,g;s,_mm_andnot_si64,simde_mm_andnot_si64,g' "$*"
 fi
