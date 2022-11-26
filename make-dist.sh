3526f5e73ba0be23ec64e59d45fa647794c083ce
hexdump <hexdump0815@googlemail.com>
2022-11-26 20:46:24 +0100

    v2.2 wip: ask before overwriting

--- make-dist.sh
+++ make-dist.sh
@@ -8,14 +8,14 @@ mkdir -p dist/plugins
 for i in CHANGELOG.md LICENSE-dist.md LICENSE-GPLv3.txt LICENSE.md res template.vcv Core.json; do  cp -r compile/Rack/$i dist; done
 cp compile/Rack/Rack dist
 cp compile/Rack/libRack.so dist
-( cd dist/plugins ; for i in ../../compile/library/repos/*/dist/*.vcvplugin ; do cp -r $i . ; done )
-( cd dist/plugins ; for i in ../../compile/plugins/*/dist/*.vcvplugin ; do cp -r $i . ; done )
+( cd dist/plugins ; for i in ../../compile/library/repos/*/dist/*.vcvplugin ; do cp -ri $i . ; done )
+( cd dist/plugins ; for i in ../../compile/plugins/*/dist/*.vcvplugin ; do cp -ri $i . ; done )
 
 mkdir -p dist/rack-sdk/dep
 for i in *.mk helper.py include LICENSE-dist.md LICENSE-GPLv3.txt LICENSE.md; do
-  cp -r compile/Rack/$i dist/rack-sdk
+  cp -ri compile/Rack/$i dist/rack-sdk
 done
 
-cp -r compile/Rack/dep/include dist/rack-sdk/dep
+cp -ri compile/Rack/dep/include dist/rack-sdk/dep
 
 cp simde-ify.sh dist/rack-sdk
