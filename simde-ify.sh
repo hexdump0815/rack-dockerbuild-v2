f45ced5f69b133e51225872c720bfa58629cbe18
hexdump <hexdump0815@googlemail.com>
2022-11-26 20:49:27 +0100

    v2.2 wip: lets try to simplify simdeify

--- simde-ify.sh
+++ simde-ify.sh
@@ -4,5 +4,5 @@ MYARCH=`uname -m`
 
 # only do this on arm
 if [ "$MYARCH" = "armv7l" ] || [ "$MYARCH" = "aarch64" ]; then
-  sed -i 's,<mmintrin\.h,<simde/x86/sse4.2.h,g;s,<xmmintrin\.h,<simde/x86/sse4.2.h,g;s,<pmmintrin\.h,<simde/x86/sse4.2.h,g;s,<emmintrin\.h,<simde/x86/sse4.2.h,g;s,<immintrin\.h,<simde/x86/sse4.2.h,g;<x86/sse3\.h,<simde/x86/sse4.2.h,g;s,__m64,simde__m64,g;s,_mm_or_si64,simde_mm_or_si64,g;s,_mm_and_si64,simde_mm_and_si64,g;s,_mm_andnot_si64,simde_mm_andnot_si64,g' "$*"
+  sed -i 's,<mmintrin\.h,<simde/x86/sse4.2.h,g;s,<xmmintrin\.h,<simde/x86/sse4.2.h,g;s,<pmmintrin\.h,<simde/x86/sse4.2.h,g;s,<emmintrin\.h,<simde/x86/sse4.2.h,g;s,<immintrin\.h,<simde/x86/sse4.2.h,g;<x86/sse3\.h,<simde/x86/sse4.2.h,g' "$*"
 fi
