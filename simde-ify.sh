d4a01326b9d0bfcb73c62b8a4735c2c9c71957e3
hexdump <hexdump0815@googlemail.com>
2022-11-26 22:06:40 +0100

    v2.2 wip: add missing command to simdeify sed

--- simde-ify.sh
+++ simde-ify.sh
@@ -4,5 +4,5 @@ MYARCH=`uname -m`
 
 # only do this on arm
 if [ "$MYARCH" = "armv7l" ] || [ "$MYARCH" = "aarch64" ]; then
-  sed -i 's,<mmintrin\.h,<simde/x86/sse4.2.h,g;s,<xmmintrin\.h,<simde/x86/sse4.2.h,g;s,<pmmintrin\.h,<simde/x86/sse4.2.h,g;s,<emmintrin\.h,<simde/x86/sse4.2.h,g;s,<immintrin\.h,<simde/x86/sse4.2.h,g;<x86/sse3\.h,<simde/x86/sse4.2.h,g' "$*"
+  sed -i 's,<mmintrin\.h,<simde/x86/sse4.2.h,g;s,<xmmintrin\.h,<simde/x86/sse4.2.h,g;s,<pmmintrin\.h,<simde/x86/sse4.2.h,g;s,<emmintrin\.h,<simde/x86/sse4.2.h,g;s,<immintrin\.h,<simde/x86/sse4.2.h,g;s,<x86/sse3\.h,<simde/x86/sse4.2.h,g' "$*"
 fi
