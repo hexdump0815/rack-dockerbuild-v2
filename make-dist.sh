cbfad6e03f35d5e105f14d994cff24707b02f57a
hexdump <hexdump0815@googlemail.com>
2022-11-27 13:04:29 +0100

    v2.2 wip: make room for beta plugins

--- make-dist.sh
+++ make-dist.sh
@@ -4,12 +4,17 @@ WORKDIR=`dirname $0`
 cd $WORKDIR
 WORKDIR=`pwd`
 
-mkdir -p dist/plugins
+mkdir -p dist
 for i in CHANGELOG.md LICENSE-dist.md LICENSE-GPLv3.txt LICENSE.md res template.vcv Core.json; do  cp -r compile/Rack/$i dist; done
 cp compile/Rack/Rack dist
 cp compile/Rack/libRack.so dist
+
+mkdir -p dist/plugins
 ( cd dist/plugins ; for i in ../../compile/library/repos/*/dist/*.vcvplugin ; do cp -ri $i . ; done )
 ( cd dist/plugins ; for i in ../../compile/plugins/*/dist/*.vcvplugin ; do cp -ri $i . ; done )
+
+mkdir -p dist/plugins-beta
+( cd dist/plugins-beta ; for i in ../../compile/plugins/*-beta/dist/*.vcvplugin ; do cp -ri $i . ; done )
 
 mkdir -p dist/rack-sdk/dep
 for i in *.mk helper.py include LICENSE-dist.md LICENSE-GPLv3.txt LICENSE.md; do
